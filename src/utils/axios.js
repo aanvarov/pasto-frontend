870e0e36ec643868165cb9997be0269681043d9f
aanvarov <anvarov2295@gmail.com>
2022-04-15 22:32:37 +0500

    final

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,40 +1,36 @@
-import Axios from "axios";
-import store from "../store";
-import { clearRestaurant, updateRestaurant } from "../store/auth/reducer";
+import Axios from 'axios';
+import store from '../store';
+import { clearRestaurant, updateRestaurant } from '../store/auth/reducer';
 
 const baseURL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:4001/api/v1"
-    : "http://localhost:4001/api/v1";
+  process.env.NODE_ENV === 'development'
+    ? 'https://yemek.alitechbot.uz/api/v1'
+    : 'https://yemek.alitechbot.uz/api/v1';
 const axios = Axios.create({ baseURL, withCredentials: true });
 
 axios.interceptors.request.use((configs) => {
-  const accessToken = store.getState().account.restaurant.accessToken || "";
-  configs.headers.authorization = accessToken ? `Bearer ${accessToken}` : "";
-  const refreshToken = store.getState().account.restaurant.refreshToken || "";
-  configs.headers["x-refresh-token"] = refreshToken
-    ? `Bearer ${refreshToken}`
-    : "";
+  const accessToken = store.getState().account.restaurant.accessToken || '';
+  configs.headers.authorization = accessToken ? `Bearer ${accessToken}` : '';
+  const refreshToken = store.getState().account.restaurant.refreshToken || '';
+  configs.headers['x-refresh-token'] = refreshToken ? `Bearer ${refreshToken}` : '';
   return configs;
 });
 
 axios.interceptors.response.use(
   (res) => {
-    console.log("axios res res", res);
-    if (res.headers["x-access-token"]) {
-      store.dispatch(
-        updateRestaurant({ accessToken: res.headers["x-access-token"] })
-      );
+    console.log('res.headers[x-access-token]', { res });
+    if (res.headers['x-access-token']) {
+      store.dispatch(updateRestaurant({ accessToken: res.headers['x-access-token'] }));
     }
     return res;
   },
   (error) => {
-    console.log("axios res error", error.response);
+    console.log('axios res error', error.response);
     if (error?.response?.status === 401) {
       return store.dispatch(clearRestaurant());
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 export { axios as default };
