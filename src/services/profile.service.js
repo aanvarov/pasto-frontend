e9a76ac87ccb717d64a022df468e84d09d9b2deb
Abdulkhamid <abdulkhamid1828@gmail.com>
2022-06-08 11:15:30 +0500

    profile get user info

---
+++ src/services/profile.service.js
@@ -0,0 +1,21 @@
+import { message } from "antd";
+import axios from "../utils/axios";
+import { getErrorMessage } from "../utils";
+
+export const GET_USER = async () => {
+  try {
+    const { data } = await axios.get("/user");
+    return data;
+  } catch (error) {
+    message.error(getErrorMessage(error));
+  }
+};
+
+export const UPDATE_USER = async (user) => {
+  try {
+    const { data } = await axios.put("/user", user);
+    return data;
+  } catch (error) {
+    message.error(getErrorMessage(error));
+  }
+};
--- src/views/Profile/Profile.style.js
+++ src/views/Profile/Profile.style.js
@@ -1,5 +1,30 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const StyledProfile = styled.div`
-margin: 0px;
-`
+  margin: 0px;
+  .user-block {
+    width: 100%;
+    max-width: 650px;
+    .user-table {
+      font-family: Arial, Helvetica, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+      tr {
+        height: 65px;
+      }
+      td {
+        border-bottom: 1px solid #ddd;
+        padding: 8px;
+      }
+      th {
+        border-bottom: 1px solid #ddd;
+        padding: 8px;
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #04aa6d;
+        color: white;
+      }
+    }
+  }
+`;
